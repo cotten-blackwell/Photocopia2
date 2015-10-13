d1eafafaec6e88554d6c91380fdbd993b8f20e05
cotten-blackwell <cotten.blackwell@gmail.com>
2015-10-13 07:21:47 -0700

    Clearing deck for pairing

--- PresentDetailTransition.m
+++ PresentDetailTransition.m
@@ -12,28 +12,28 @@
 
 //TODO -- fix during AirPair session 0....
 
--(void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
-    UIViewController *detail = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
-    UIView *containerView = [transitionContext containerView];
-    
-    detail.view.alpha = 0.0;
-    
-    CGRect frame = containerView.bounds;
-    frame.origin.y += 20.0;
-    frame.size.height -= 20.0;
-    detail.view.frame = frame;
-    [containerView addSubview:detail.view];
-    
-    [UIView animateWithDuration:0.3 animations:^{
-        detail.view.alpha = 1.0;
-    } completion:^(BOOL finished) {
-        [transitionContext completeTransition:YES];
-    }];
-}
-
--(NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext {
-    return 0.3;
-}
+//-(void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
+//    UIViewController *detail = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+//    UIView *containerView = [transitionContext containerView];
+//    
+//    detail.view.alpha = 0.0;
+//    
+//    CGRect frame = containerView.bounds;
+//    frame.origin.y += 20.0;
+//    frame.size.height -= 20.0;
+//    detail.view.frame = frame;
+//    [containerView addSubview:detail.view];
+//    
+//    [UIView animateWithDuration:0.3 animations:^{
+//        detail.view.alpha = 1.0;
+//    } completion:^(BOOL finished) {
+//        [transitionContext completeTransition:YES];
+//    }];
+//}
+//
+//-(NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext {
+//    return 0.3;
+//}
 
 
 @end
